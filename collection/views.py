1f578df4d186ffb693b4da39c96fc40bbc7221a1
Josh <joshchamp@gmail.com>
2017-03-01 21:55:53 -0800

    commit

--- collection/views.py
+++ collection/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime, timedelta
 from django import forms
-from chartit import PivotDataPool, PivotChart
+
 
 @login_required	
 def home(request):
